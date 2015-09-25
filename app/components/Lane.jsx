75fa070badd54e1747ccb443ac0ff53fc89d8b37
Yury Bychkov <graywizard@gmail.com>
2015-09-24 21:12:45 -0700

    Added Drag & Drop

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -5,8 +5,27 @@ import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
 import Editable from './Editable.jsx';
+import {DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-class Lane extends React.Component {
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if(!targetProps.notes.length) {
+      LaneActions.attachToLane({
+        laneId: targetProps.id,
+        noteId: sourceId
+      });
+    }
+  }
+};
+
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Lane extends React.Component {
   constructor(props) {
     super(props);
 
@@ -17,9 +36,9 @@ class Lane extends React.Component {
     this.editName = this.editName.bind(this, id);
   }
   render() {
-    const {id, name, notes, ...props} = this.props;
+    const {connectDropTarget, id, name, notes, ...props} = this.props;
 
-    return (
+    return connectDropTarget(
       <div {...props}>
         <div className='lane-header'>
           <Editable className='lane-name' value={name}
@@ -61,5 +80,3 @@ class Lane extends React.Component {
     }
   }
 }
-
-export default Lane;
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -2,6 +2,7 @@ import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
 import NoteStore from './NoteStore';
+import update from 'react/lib/update';
 
 class LaneStore {
   constructor() {
@@ -55,7 +56,7 @@ class LaneStore {
     if(targetId < 0) {
       return;
     }
-
+    this.removeNote(noteId);
     const lane = lanes[targetId];
 
     if(lane.notes.indexOf(noteId) === -1) {
@@ -67,6 +68,21 @@ class LaneStore {
       console.warn('Already attached note to lane', lanes);
     }
   }
+  removeNote(noteId) {
+    const lanes = this.lanes;
+    const removeLane = lanes.filter((lane) => {
+      return lane.notes.indexOf(noteId) >= 0;
+    })[0];
+
+    if(!removeLane) {
+      return;
+    }
+
+    const removeNoteIndex = removeLane.notes.indexOf(noteId);
+
+    removeLane.notes = removeLane.notes.slice(0, removeNoteIndex).
+      concat(removeLane.notes.slice(removeNoteIndex + 1));
+  }  
   detachFromLane({laneId, noteId}) {
     const lanes = this.lanes;
     const targetId = this.findLane(laneId);
@@ -99,6 +115,36 @@ class LaneStore {
 
     return laneIndex;
   }
+  move({sourceId, targetId}) {
+    const lanes = this.lanes;
+    const sourceLane = lanes.filter((lane) => {
+      return lane.notes.indexOf(sourceId) >= 0;
+    })[0];
+    const targetLane = lanes.filter((lane) => {
+      return lane.notes.indexOf(targetId) >= 0;
+    })[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+    if(sourceLane === targetLane) {
+      // move at once to avoid complications
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    }
+    else {
+      // get rid of the source
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+
+      // and move it to target
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
+
+    this.setState({lanes});
+  } 
 }
 
 export default alt.createStore(LaneStore, 'LaneStore');
